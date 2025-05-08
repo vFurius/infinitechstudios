1300c78b96044ed75ddb38248a583c0dd211ecea
vFurius <axelcastro7542@gmail.com>
2025-05-08 11:06:44 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import LoadingScreen from "@/components/LoadingScreen";
-import "./page.module.css";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
